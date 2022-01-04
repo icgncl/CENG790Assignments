ed02dc18d2487c53c91d247570135f548dde4a4a
İbrahim Can Gençel <ibrahimcangencel@gmail.com>
2022-01-04 21:35:44 +0300

    Assignment III is completed

--- AssignmentIII/src/main/scala/edu/metu/ceng790/hw3/Credit.scala
+++ AssignmentIII/src/main/scala/edu/metu/ceng790/hw3/Credit.scala
@@ -1,10 +1,13 @@
 package edu.metu.ceng790.hw3
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
 
 object Credit {
   // define the Credit Schema
@@ -44,13 +47,13 @@ object Credit {
 
     val sc = spark.sparkContext
 
-    // load the data into a  RDD
+    // load the data into a RDD
     val creditRDD = parseRDD(sc.textFile("credit/credit.csv")).map(parseCredit)
 
-    // PART 1 //
     // Converting RDD to DF
     val creditDF = spark.createDataFrame(creditRDD)
 
+    // PART 1 //
     // Vector Assembler
     val vector_assembler = new VectorAssembler().
       setInputCols(
@@ -65,9 +68,63 @@ object Credit {
       .setOutputCol("creditabilityIndex")
 
     // PART 3 //
+    // Train test split with ratio %90 to %10 and seed was set
+    // in order to get same test and train set in each run
     val Array(train_set, test_set) = creditDF.randomSplit(Array[Double](0.9, 0.1), seed = 18)
 
     // PART 4 //
+    // train Random Forest model with training data set
+    val rfc = new RandomForestClassifier()
+      .setSeed(1234)
+      .setFeaturesCol("all_features")
+      .setLabelCol("creditabilityIndex")
+      .setFeatureSubsetStrategy("auto")
+
+    // grid search parameters are given
+    val grid_search = new ParamGridBuilder()
+      .addGrid(rfc.maxDepth, Array(4, 6, 8))
+      .addGrid(rfc.maxBins, Array(25, 28, 31))
+      .addGrid(rfc.impurity, Array("entropy", "gini"))
+      .build()
+
+    // PART 5 //
+    // Pipeline object is created
+    val pipeline = new Pipeline()
+      .setStages(Array(vector_assembler, creditability_indexer, rfc))
+
+
+    // Creating train validation split
+      // BinaryClassificationEvaluator is used
+      // 0.75 ratio means 3x -> train set x -> validation set
+    val train_val_split = new TrainValidationSplit()
+      .setEstimator(pipeline)
+      .setEstimatorParamMaps(grid_search)
+      .setEvaluator(new BinaryClassificationEvaluator().setLabelCol("creditabilityIndex"))
+      .setTrainRatio(0.75)
+
+    // Training the model
+    val model_fit = train_val_split.fit(train_set)
+
+    // Getting the best parameters
+    // I could not find the best parameters because I have used Pipeline
+    println(model_fit.bestModel.extractParamMap())
+
+    // PART 6 //
+    // Make predictions.
+    val predictions_test = model_fit.transform(test_set)
+    val predictions_train = model_fit.transform(train_set)
+
+    val evaluator = new BinaryClassificationEvaluator()
+      .setLabelCol("creditabilityIndex")
+
+    val accuracy_test = evaluator.evaluate(predictions_test)
+    val accuracy_train = evaluator.evaluate(predictions_train)
+    println(s"Train Accuracy = ${accuracy_train}")
+    println(s"Test Accuracy = ${accuracy_test}")
+
+
+
+
 
     spark.stop()
 
